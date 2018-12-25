3437b5535b95f5265567b89e26fe535bbb54c525
Juan Redondo <juanantonioredondo@macbook-pro-de-juan.local>
2018-12-25 19:42:53 +0100

    Initial Commit

---
+++ 8MUSIC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8MUSIC
+//
+//  Created by Juan Antonio Redondo on 25/12/2018.
+//  Copyright © 2018 Juan Antonio Redondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
