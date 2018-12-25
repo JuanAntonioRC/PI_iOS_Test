03f670d85795722be7e87b0f6e63f81e90202657
Juan Redondo <juanantonioredondo@macbook-pro-de-juan.local>
2018-12-25 20:28:03 +0100

    Primer commit con la interfaz

--- 8MUSIC/ViewController.swift
+++ 8MUSIC/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //variables de input
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var passInput: UITextField!
+    
+    
+    @IBAction func buttonLogin(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+    }
+    
+    @IBAction func passRecoveryButton(_ sender: Any) {
+    }
+    
+    @IBAction func createUserButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
